6f048e256406a48e7aaa66508c577f095f40618e
Jean Ruggiero <jeanruggiero@gmail.com>
2019-06-03 20:44:56 -0700

    RandomComic complete

---
+++ RandomComic/RandomComic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using xkcd;
+
+namespace RandomComic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hit 'Enter' for a random comic!");
+            while(true)
+            {
+                Console.ReadLine();
+                Comic comic = new Comic();
+                Console.WriteLine('"' + comic.title + '"' + " published on " +
+                    comic.published_date + ':');
+                Console.WriteLine();
+                Console.WriteLine(comic.transcript);
+            }
+        }
+    }
+}
